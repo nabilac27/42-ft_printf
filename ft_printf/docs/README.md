9d395e7e84255d61254c292bc520cce7def6a771
Nabila Chairunnisa <nchairun@3-F-4.42heilbronn.de>
2025-04-01 11:08:21 +0200

    improved

---
+++ ft_printf/docs/README.md
@@ -0,0 +1,25 @@
+This repository contains my implementation of the ft_printf function, 
+a fundamental part of the 42 School curriculum.
+
+The goal is to recreate the standard C printf function with specified 
+features and constraints while deepening understanding of variadic functions, 
+memory management, and formatted output.
+
+About ft_printf
+
+  printf is a variadic function, meaning it accepts a variable number of arguments. 
+  The first argument is a format string that defines how the subsequent arguments are formatted and displayed.
+
+Key Concepts
+
+  Variadic Functions
+
+  The <stdarg.h> library provides macros for working with variadic functions:
+    va_start: Initializes a va_list for use.
+    va_arg: Retrieves the next argument in the list.
+    va_end: Cleans up the va_list when done.
+
+  Return Type
+
+  Returns the total length of characters printed.
+  If an error occurs, ft_printf returns -1.
---
+++ ft_printf/main.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_function.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchairun <nchairun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 01:11:06 by nchairun          #+#    #+#             */
+/*   Updated: 2024/11/14 01:20:45 by nchairun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+// int	main(void)
+// {
+// 	// String, char, integer
+// 	printf("printf: \n");
+// 	printf("%d,%d,%d Hello %s%c", 1,2,3, "World", '!'); // Input
+// 	printf("\n\n");
+
+// 	ft_printf("ft_printf\n");
+// 	ft_printf("%d,%d,%d Hello %s%c", 1,2,3, "World", '!'); // Input
+// 	ft_printf("\n\n--------- \n");
+
+// 	// Pointer address in Hexadecimal
+// 	int test_ptr = 2024;
+// 	printf("printf: \n");
+// 	printf("The address of this variable: %p", &test_ptr); // Input
+// 	printf("\n\n");
+
+// 	ft_printf("ft_printf\n");
+// 	ft_printf("The address of this variable: %p", &test_ptr); // Input
+// 	ft_printf("\n\n--------- \n");
+
+// 	// Hexadecimal
+// 	int test_hex = 2024;
+// 	printf("printf: \n");
+// 	printf("The hexadecimal of given number: %X", test_hex); // Input
+// 	printf("\n\n");
+
+// 	ft_printf("ft_printf\n");
+// 	ft_printf("The hexadecimal of given number: %X", test_hex); // Input
+// 	ft_printf("\n");
+
+// 	// Unsigned int
+// 	int test_u = -123;
+// 	printf("printf: \n");
+// 	printf("The unsgined int of given number: %u", test_u); // Input
+// 	printf("\n\n");
+
+// 	ft_printf("ft_printf\n");
+// 	ft_printf("The unsigned int of given number: %u", test_u); // Input
+// 	ft_printf("\n");
+
+// 	// // Other test
+// 	// printf("printf: \n");
+// 	// printf("   "); // Input
+// 	// printf("\n\n");
+
+// 	// ft_printf("ft_printf\n");
+// 	// ft_printf(   ""); // Input
+// 	// ft_printf("\n\n--------- \n");
+
+// 	return (0);
+// }
+
+/*
+	cc ft_printf.c ft_printf_digits.c ft_printf_hex.c ft_printf_words.c
+	main_function.c
+*/
